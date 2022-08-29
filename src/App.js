c54740ea1bd58739972471248a28b6e25b570ab0
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-08-29 17:25:26 -0400

    installed axios, subscribed to api, superhero api hooked up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Heroes from './components/Heroes';
 
 function App() {
   return (
     <div className="App">
+    <Heroes />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
